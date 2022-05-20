10a73a45bff7772c76ed6d08b90fa41cdf8b3b21
isaacbionicle <101652530+isaacbionicle@users.noreply.github.com>
2022-05-19 22:25:18 -0500

    cambio completo

--- ITABARATO/administracion/categorias/formaniadircategorias.php
+++ ITABARATO/administracion/categorias/formaniadircategorias.php
@@ -13,7 +13,7 @@ session_start();
 <link rel="stylesheet" href="../administracion.css">
 
 <!-- Start css3menu.com HEAD section -->
-<link rel="stylesheet" href="../CSS3 Menu.css3prj_files/css3menu1/style.css" type="text/css" /><style type="text/css">._css3m{display:none}</style>
+<link rel="stylesheet" href="../css.css">
 <!-- End css3menu.com HEAD section -->
 
 <!-- bootstrap -->
@@ -57,23 +57,20 @@ function eliminar(id){//parametro id de la categoria eliminar
 
 <body>
 
-<div class="cabecera2"></div>
 
-<div style=" text-align:center; margin-top:-75px; margin-bottom:10px">
 <!-- Start css3menu.com BODY section -->
-<input type="checkbox" id="css3menu-switcher" class="c3m-switch-input">
-<ul id="css3menu1" class="topmenu">
-	<li class="switch"><label onclick="" for="css3menu-switcher"></label></li>
-	<li class="topmenu"><a href="../pedidos/ver_pedidos.php" style="width:157px;height:55px;line-height:55px;"><span style="margin-top:-15px">Pedidos</span></a></li>
-	<li class="topmenu"><a href="../productos/mostrarproductos.php" style="width:170px;height:55px;line-height:55px;"><span style="margin-top:-15px">Productos</span></a>
-	<ul>
-		<li><a href="../productos/formaniadirproductos.php">Añadir Producto </a></li>
-	</ul></li>
-	<li class="topmenu"><a class="pressed" href="#" style="width:180px;height:55px;line-height:55px;"><span style="margin-top:-15px"><span style="margin-top:-15px">Categorias</span></a></li>
-	<li class="topmenu"><a href="../clientes/" style="width:157px;height:55px;line-height:55px;"><span style="margin-top:-15px">Clientes</span></a></li>
-	<li class="topmenu"><a href="../chat" style="width:158px;height:55px;line-height:55px;"><span style="margin-top:-15px">Chat</span></a></li>
-    <li class="topmenu"><a href="../comentarios/mostrar_comentarios.php" style="width:190px;height:55px;line-height:55px;"><span style="margin-top:-15px">Comentarios</span></a></li>
-</ul>
+<input class="menu-icon" type="checkbox" id="menu-icon" name="menu-icon"/>
+  	<label for="menu-icon"></label>
+  	<nav class="nav"> 		
+  		<ul class="pt-5">
+		  <li><a href="../pedidos/ver_pedidos.php">Pedidos</a></li>
+  			<li><a href="../productos/mostrarproductos.php">Productos</a></li>
+  			<li><a href="../productos/formaniadirproductos.php">Añadir productos</a></li>
+			<li><a href="../categorias/formaniadircategorias.php">Categorias</a></li>
+			<li><a href="../clientes/">Clientes</a></li>
+			<li><a href="../comentarios/mostrar_comentarios.php">Comentarios</a></li>
+  		</ul>
+  	</nav>
 <!-- End css3menu.com BODY section -->
 </div>
 
