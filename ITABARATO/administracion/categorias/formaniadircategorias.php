2378b78ea3b704066df7aeb84dca36eece537af9
isaacbionicle <101652530+isaacbionicle@users.noreply.github.com>
2022-05-20 17:09:15 -0500

    Add files via upload

--- ITABARATO/administracion/categorias/formaniadircategorias.php
+++ ITABARATO/administracion/categorias/formaniadircategorias.php
@@ -68,6 +68,7 @@ function eliminar(id){//parametro id de la categoria eliminar
   			<li><a href="../productos/formaniadirproductos.php">Añadir productos</a></li>
 			<li><a href="../categorias/formaniadircategorias.php">Categorias</a></li>
 			<li><a href="../clientes/">Clientes</a></li>
+			<li><a href="../pedidos/ver_estadistica.php">Estadisticas</a></li>
 			<li><a href="../comentarios/mostrar_comentarios.php">Comentarios</a></li>
   		</ul>
   	</nav>
